d574d49c6d285e4202e9aa2ef27b4e35b5f4cdd8
IJAJ MULANI <ijajmulani786@gmail.com>
2016-08-15 09:52:05 +0530

    code refactored

--- githubapiapp/views.py
+++ githubapiapp/views.py
@@ -18,10 +18,13 @@ def index(request):
 def search_repos(request):
   postData = request.GET
   searchKey = postData.get('key')
+  repoItem = {}
 
   r = requests.get('https://api.github.com/search/repositories?q='+searchKey)
   if(r.ok):
     repoItem = json.loads(r.text or r.content)
+  else:
+    repoItem['items'] = {}
     
   return render_to_response( 'search_repos.html', {'repositories' : repoItem['items']}, RequestContext(request) )
 
@@ -30,22 +33,28 @@ def search_issues(request):
   searchKey = postData.get('key')
   repoItem = {}
   now = datetime.datetime.now()
-  
+
   r = requests.get('https://api.github.com/search/issues?q=+state:open+created:'+ now.strftime("%Y-%m-%d") +'+type:issue+repo:'+searchKey)
   if(r.ok):
     repoItem = json.loads(r.text or r.content)
-  
+  else:
+    repoItem['items'] = null
+
   return render_to_response( 'search_issues.html', {'issues' : repoItem['items'], 'reponame' : searchKey }, RequestContext(request) )
 
 def issue(request, owner, reponame, number):
+  repoItem = {}
   r = requests.get('https://api.github.com/repos/' + owner + "/" + reponame + '/issues/' + number)
   if(r.ok):
     repoItem = json.loads(r.text or r.content)
+  else:
+    repoItem['created_at'] = null
     
   repoItem['created_at'] = dateutil.parser.parse(repoItem['created_at'])
   return render_to_response( 'issue.html', {'issue' : repoItem}, RequestContext(request) )
 
 def user(request, userid):
+  repoItem = {}
   r = requests.get('https://api.github.com/users/' + userid)
   if(r.ok):
     repoItem = json.loads(r.text or r.content)
